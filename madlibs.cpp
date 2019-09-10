a0e96927f4232a31643baf1ab15239feabc4f4fd
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-09-09 23:58:40 -0700

    creates mad lib story

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,37 @@
-//Authors: 
+// Valeria Corona
 #include <iostream>
-#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::endl;
 
 int main()
 {
+    string adj1= " ";
+    string verb= " ";
+    string adj2= " ";
+    string noun= " ";
+    int number= ' ';
+    string adj3= " ";
+    cout << "Enter an adjective: ";
+    cin  >> adj1;
+    cout << "Enter a verb: ";
+    cin  >> verb;
+    cout << "Enter another adjective: ";
+    cin  >> adj2;
+    cout << "Enter a noun: ";
+    cin  >> noun;
+    cout << "Enter a number: ";
+    cin  >> number;
+    cout << "Enter another adjective: ";
+    cin  >> adj3;
 
-  return 0;
+    cout << "Your Mad Libs Story:" <<endl;
+    cout << "It was a " << adj1 << " September morning. I " << verb << "\n"
+         << "up to the " << adj2 << " smell of " << noun << " in the "
+         << "kitchen.\nThere were " << number << " " << adj3 << " strangers \n"
+         << "sitting at the table." <<endl;
+
+    return 0;
 }
